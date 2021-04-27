ff790db4618e412730874f78938452a607aca3c4
Anz MU <anz_mu@mail.ru>
2021-04-27 15:03:22 +0300

    First Commit

---
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.calc
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val secondRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val secondDiceImage: ImageView = findViewById(R.id.imageView2)
+        val drawRes = when (diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> 0
+        }
+        val secondDrawRes = when (secondRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> 0
+        }
+        secondDiceImage.setImageResource(secondDrawRes)
+        secondDiceImage.contentDescription = secondRoll.toString()
+        diceImage.setImageResource(drawRes)
+        diceImage.contentDescription = diceRoll.toString()
+
+    }
+
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+
+    }
+}
